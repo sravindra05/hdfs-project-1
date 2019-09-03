bde7e58a5805f05d6ccd664112ce498cf013c0d1
Pragnya Sridhar <pragnya1998@gmail.com>
2019-09-03 20:03:27 +0530

    Tested on desktop

--- mapper_t2.py
+++ mapper_t2.py
@@ -4,18 +4,21 @@ import csv
 infile = sys.stdin
 #next(infile)
 
-#fuel column index 8
 for line in infile:
     line = line.strip()
     my_list = line.split(',')
+
+    # avoid lines starting with version and info to process
     if(my_list[0]=='ball'):
-        val=[1]
-        key=(my_list[4],my_list[6])
-        val.append(my_list[7])
-        if(my_list[9]!="" and my_list[9]!="run out"):
-            val.append(0)
+        # val= [no_of_deliveries, no_of_runs, no_of_wickets]
+        val=[1] # no_of_deliveries for 1 record
+        key=(my_list[4],my_list[6]) # batsman in column 4 and bowler in column 6
+        val.append(int(my_list[7])) # no_of_runs in record
+        #ensuring that runouts and no wickets are not counted
+        if(my_list[9]!='""' and my_list[9]!="run out"): 
+            val.append(1) 
         else:
-            val.append(1)
+            val.append(0)
         print('%s:%s' % (str(key),str(val)))
     
 
--- reducer_t2.py
+++ reducer_t2.py
@@ -3,33 +3,52 @@ import csv
 from operator import itemgetter
 import sys
 
-# current_count = 0
-# current_key = ""
+# resultant dictionary
 res=dict()
 for line in sys.stdin:
     line = line.strip()
     line_val = line.split(":")
     key, val = line_val[0], line_val[1]
+    # print(type(key),type(val))
     try:
+        # formatting key
+        key=key.split("(")
+        key=key[1]
+        key=key.split(")")
+        key=key[0]
+        key=key.split(",")
+        key[1]=key[1][1::] # removes the first element " " (space)
+
+        # formatting val
         val=val.split("[")
         val=val[1]
         val=val.split("]")
         val=val[0]
         val=val.split(",")
+
+        
         ball_count = int(val[0]) 
         run_count = int(val[1])
         wicket_count = int(val[2]) 
+
     except ValueError:
+        print("ValueError")
         continue
-    # global current_key
-    # global current_count
+
+    # removing single quotes from names of batsman and bowler
+    key[0]=key[0][1:len(key[0])-1]
+    key[1]=key[1][1:len(key[1])-1]
+
+    key=tuple(key)
+    
     if(key in res.keys()):
         res[key][0]+=ball_count
         res[key][1]+=run_count
         res[key][2]+=wicket_count
     else:
         res.update({key:[ball_count,run_count,wicket_count]})
-    
+
+
 for key in res.keys():
     if(res[key][0]>5):
-        print('%s,%s,%s,%s' % (key[0],key[1], res[key][2],res[key][0]))
+        print('%s,%s,%s,%s' % (str(key[0]),str(key[1]), str(res[key][2]),str(res[key][0])))
