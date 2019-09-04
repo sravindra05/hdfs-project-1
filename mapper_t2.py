2ab9817316233850e8792cce1034043a1b891476
Pragnya Sridhar <pragnya1998@gmail.com>
2019-09-04 21:50:44 +0530

    task 2 modified

--- mapper_t2.py
+++ mapper_t2.py
@@ -13,7 +13,7 @@ for line in infile:
         # val= [no_of_deliveries, no_of_runs, no_of_wickets]
         val=[1] # no_of_deliveries for 1 record
         key=(my_list[4],my_list[6]) # batsman in column 4 and bowler in column 6
-        val.append(int(my_list[7])) # no_of_runs in record
+
         #ensuring that runouts and no wickets are not counted
         if(my_list[9]!='""' and my_list[9]!="run out"): 
             val.append(1) 
--- reducer_t2.py
+++ reducer_t2.py
@@ -28,8 +28,7 @@ for line in sys.stdin:
 
         
         ball_count = int(val[0]) 
-        run_count = int(val[1])
-        wicket_count = int(val[2]) 
+        wicket_count = int(val[1]) 
 
     except ValueError:
         print("ValueError")
@@ -43,17 +42,16 @@ for line in sys.stdin:
     
     if(key in res.keys()):
         res[key][0]+=ball_count
-        res[key][1]+=run_count
-        res[key][2]+=wicket_count
+        res[key][1]+=wicket_count
     else:
-        res.update({key:[ball_count,run_count,wicket_count]})
+        res.update({key:[ball_count,wicket_count]})
 
-# -x[1][2] for descending order of wickets
+# -x[1][1] for descending order of wickets
 # x[1][0] for increasing order of balls played in case of tie of above
 # x[0][0] for alphabetic order of name of batsman
-res=sorted(res.items(),key= lambda x : (-x[1][2] , x[1][0], x[0][0]),reverse=False)
+res=sorted(res.items(),key= lambda x : (-x[1][1] , x[1][0], x[0][0]),reverse=False)
 
 # output format: batsman,bowler,no_of_wickets,no_of_deliveries
 for rec in res:
     if(rec[1][0]>5):
-        print('%s,%s,%d,%d' % (rec[0][0],rec[0][1],rec[1][2],rec[1][0]))
+        print('%s,%s,%d,%d' % (rec[0][0],rec[0][1],rec[1][1],rec[1][0]))
